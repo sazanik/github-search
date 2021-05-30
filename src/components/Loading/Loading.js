e768cf7f05bfc72e69c1e256498c876a7ba47d23
kingurru <kingnikoriginal@gmail.com>
2021-05-30 16:50:15 +0300

    feat:add loading

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './Loading.scss'
+
+const Loading = () => (
+  <div className="lds-hourglass"/>
+)
+
+export default Loading
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,28 @@
 import React from "react";
 import '../App.scss'
 import icon from '../icons/search-big.svg'
+import {useGithubContext} from "../context/github/state";
+import Loading from "../components/Loading/Loading";
 
 const Home = () => {
+
+  const {getUser, loading} = useGithubContext()
+
   return (
-    <div className='container'>
-      <img
-        className='icon'
-        src={icon}
-        alt="icon"
-      />
-      <p className='text text-home'
-      >
-        Start with searching
-        a GitHub user
-      </p>
-    </div>
+    loading
+      ? <Loading/>
+      : <div className='container'>
+        <img
+          className='icon'
+          src={icon}
+          alt="icon"
+        />
+        <p className='text text-home'
+        >
+          Start with searching
+          a GitHub user
+        </p>
+      </div>
   )
 }
 
