ef35f8a01e2d869d8650ae2d62ad4cd4b3d0788f
kingurru <kingnikoriginal@gmail.com>
2021-05-31 17:09:09 +0300

    end paginate

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from "react";
+import './Pagination.scss'
+import {useGithubContext} from "../../context/github/state";
+import Loading from "../Loading/Loading";
+
+const Pagination = (props) => {
+
+  const [firstRender, setFirstRender] = useState(true)
+
+  const {currentPage, perPage, setCurrentPage, getRepos, memo, loading, totalRepos} = useGithubContext()
+
+  const pagesCount = Math.ceil(totalRepos / perPage)
+  const pagesArr = []
+
+
+  const createPages = (pagesArr, pagesCount, currentPage) => {
+    if (pagesCount > 10) {
+      if (currentPage > 5) {
+        for (let i = currentPage - 4; i <= currentPage + 5; i++) {
+          pagesArr.push(i)
+          if (i === pagesCount) break
+        }
+      } else {
+        for (let i = 1; i <= 10; i++) {
+          pagesArr.push(i)
+          if (i === pagesCount) break
+        }
+      }
+    } else {
+      for (let i = 1; i <= pagesCount; i++) {
+        pagesArr.push(i)
+      }
+    }
+  }
+  createPages(pagesArr, pagesCount, currentPage)
+
+  useEffect(() => {
+    if (!firstRender) {
+      setFirstRender(false)
+      getRepos(memo, perPage, currentPage)
+      createPages(pagesArr, pagesCount, currentPage)
+    }
+  }, [currentPage])
+
+  const clickHandler = (memo, perPage, currentPage) => {
+
+    if (currentPage === 0 || currentPage === pagesCount + 1) return
+    setCurrentPage(currentPage)
+    getRepos(memo, perPage, currentPage)
+    createPages(pagesArr, pagesCount, currentPage)
+  }
+
+  return (
+    loading
+      ? <Loading/>
+      : pagesArr.length < 2
+      ? null
+      : <div className='Pagination'>
+          <span className='one-step'
+                onClick={() => clickHandler(memo, perPage, currentPage - 1)}>{'<'}</span>
+        {pagesArr.map(page => (
+          <span
+            className={currentPage === page ? 'current-page' : 'page'}
+            key={Math.random()}
+            onClick={() => clickHandler(memo, perPage, page)}
+          >{page}</span>
+        ))}
+        <span className='one-step'
+              onClick={() => clickHandler(memo, perPage, currentPage + 1)}>{'>'}</span>
+      </div>
+  )
+}
+
+export default Pagination
--- src/components/Repos/Repos.js
+++ src/components/Repos/Repos.js
@@ -1,30 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import './Repos.scss'
 import {useGithubContext} from "../../context/github/state";
 import Loading from "../Loading/Loading";
+import Pagination from "../Pagination/Pagination";
 
 
 const Repos = () => {
 
-  const [firstRender, setFirstRender] = useState(true)
 
-  const {repos, user, currentPage, perPage, setCurrentPage, getRepos, memo, loading} = useGithubContext()
+  const {repos, user, loading,} = useGithubContext()
 
-  const pages = [1, 2, 3, 4, 5, 6]
-
-  useEffect(() => {
-    if (!firstRender) {
-      setFirstRender(false)
-      getRepos(memo, perPage, currentPage)
-    } else {
-      console.log('SECOND render')
-    }
-  }, [currentPage])
-
-  const clickHandler = (memo, perPage, currentPage) => {
-    setCurrentPage(currentPage)
-    getRepos(memo, perPage, currentPage)
-  }
 
   return (
     loading
@@ -32,26 +17,15 @@ const Repos = () => {
       : <>
         <div className='Repos'>
           <span>Repositories ({user.public_repos})</span>
-          {repos.map((repo, idx) => (
+          {repos.map(repo => (
               <div className='repo' key={repo.id}>
                 <a href={repo.html_url} rel="noreferrer" target='_blank'>{repo.name}</a>
                 <span>{repo.description}</span>
               </div>
-
             )
           )}
         </div>
-        <div className='pages'>
-          {pages.map((page, idx) => (
-            <span
-              className={currentPage === page ? 'current-page' : 'page'}
-              key={Math.random()}
-              onClick={() => clickHandler(memo, perPage, page)}
-            >
-            {page}
-          </span>
-          ))}
-        </div>
+        <Pagination/>
       </>
   )
 }
